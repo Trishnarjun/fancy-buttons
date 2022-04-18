88b7d2a77f1ed435068842ca8cb40fc29e5b31dd
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-18 13:25:28 -0400

    updating sates to clean up JSX for text repeat button

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -6,8 +6,11 @@ function TextRepeaterButton(){
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text</span>);
   }
+  const handleClick = () => {
+    setRepetitions(repetitions + 1);
+  }
   return (
-    <button onClick={() => setRepetitions(repetitions + 1)} className="TextRepeaterButton">
+    <button onClick={handleClick} className="TextRepeaterButton">
       {textArray}
     </button>
   );
