46bace453c16f66f66dee834eb2a840f1ae4f2c2
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-18 13:00:11 -0400

    logic for light switch turning on and of with button click

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,10 +1,18 @@
 import { useState } from "react";
 
 function LightSwitchButton(){
-  const [light, setLight] = useState();
+  let [light, setLight] = useState(0);
+  console.log(`light:`,light)
+  if (light !== 1) {
+    return (
+      <button onClick={() => setLight(light + 1)} className="LightSwitchButton">
+        {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
+      </button>
+    );
+  }
+
   return (
-    <button className="LightSwitchButton">
-      {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
+    <button onClick={() => setLight(light = 0)} className="LightSwitchButton">
       {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
     </button>
   );
