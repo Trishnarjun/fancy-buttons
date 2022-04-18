4d1f5d4757ebfeef49f1dfc5f38d2b49ee46e05b
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-18 12:49:23 -0400

    adding compents, angry button working,and counter button working

---
+++ src/components/AngryButton.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function AngryButton(){
+  let [anger, setAnger] = useState(0);
+  if (anger > 1.1) {
+    anger = 0;
+  } 
+  console.log(anger)
+  return (
+    <button onClick={() => setAnger(anger + 0.1)} style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+      {/* If you have NOT reached the maximum */}{anger < 1 && <span>Don't click me too much! </span>}
+      {/* If you HAVE reached the maximum */}{anger > 1 && <span>Rawr!</span>}
+    </button>
+  );
+}
+
+export default AngryButton;
---
+++ src/components/TextRepeaterButton.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function TextRepeaterButton(){
+  const [repetitions, setRepetitions] = useState();
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
