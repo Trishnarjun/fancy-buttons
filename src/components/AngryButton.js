a6a5cf1bb18e9fe02b9bedd03863b0c9526659d6
trishnarjun kunamalla <trishnarjun.k@gmail.com>
2022-04-18 13:20:59 -0400

    updating sates to clean up JSX for angry button

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 function AngryButton(){
   let [anger, setAnger] = useState(0);
-  if (anger > 1.1) {
-    setAnger(0);
-  } 
-  console.log(anger)
+  const handleClick = () => {
+    if (anger > 1.1) {
+      setAnger(0);
+    } else {
+      setAnger(anger + 0.1)
+    }
+  }
   return (
-    <button onClick={() => setAnger(anger + 0.1)} style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+    <button onClick={handleClick} style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
       {/* If you have NOT reached the maximum */}{anger < 1 && <span>Don't click me too much! </span>}
       {/* If you HAVE reached the maximum */}{anger > 1 && <span>Rawr!</span>}
     </button>
